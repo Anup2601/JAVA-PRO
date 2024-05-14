3740fefd7bd3c3c72c2ae6a55ce1684b068b303f
Anup Mishra <anupm0873@gmail.com>
2024-05-14 19:39:49 +0530

    typecasting2

---
+++ A5.java
@@ -0,0 +1,11 @@
+class A5
+{
+    public static void main(String[] args) {
+        int a=100,b=320;
+        byte x,y;
+        x=(byte)a;
+        y=(byte)b;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
