91740484a5ab010ec979e2ae86e7e90acce76bcc
Anup Mishra <anupm0873@gmail.com>
2024-05-14 19:32:57 +0530

    typecasting

---
+++ A4.java
@@ -0,0 +1,14 @@
+// typecasting
+class A4
+{
+    public static void main(String args[])
+    {
+        float x=23.67F;
+        double y;
+        y=x;
+        x=(float)y;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
