9a1c42c4c4a1a6dc7f840ce0086b12dc0c3efcaa
Anup Mishra <anupm0873@gmail.com>
2024-05-15 02:39:07 +0530

    local variable and garbage

---
+++ A15.java
@@ -0,0 +1,13 @@
+class A15{
+    static int x;
+    int y;
+    public static void main(String[] args) {
+        System.out.println(x);
+        // System.out.println(y);
+        A15 a=new A15();
+        System.out.println(a.y);
+        int z;
+        z=12;
+        System.out.println(z);
+    }
+}
