88f9986b1ddb7a47a3e035678e1d9d93f9d6cfe9
Anup Mishra <anupm0873@gmail.com>
2024-05-14 15:34:19 +0530

    back slace operation

---
+++ A1.java
@@ -0,0 +1,9 @@
+class A1
+{
+    public static void main(String args[])
+    {
+        System.out.print("he\nllo java1\n");
+        System.out.println("hello java 2");
+        System.out.print("hello java 3");
+    }
+}
