b297084bce54650ee63ea6bd7822cccca9e978fd
Anup Mishra <anupm0873@gmail.com>
2024-05-14 19:26:49 +0530

    conconitation

---
+++ A3.java
@@ -0,0 +1,16 @@
+// concetation
+class A3
+{
+    public static void main(String args[])
+    {
+        int a=56;
+        double b=23.68;
+        System.out.println("hello");
+        System.out.println(10+65);
+        System.out.println("hello"+"ABC");
+        System.out.println("A="+a);
+        System.out.println("value of b is "+b);
+        System.out.println("B="+b);
+        System.out.println("value of b is "+b);
+    }
+}
