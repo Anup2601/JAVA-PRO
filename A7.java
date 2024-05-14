040b3f06658bbf7e32aab5495eaa97c869bd1e87
Anup Mishra <anupm0873@gmail.com>
2024-05-14 19:47:28 +0530

    use of operator

---
+++ A7.java
@@ -0,0 +1,10 @@
+class A7{
+    public static void main(String[] args) {
+        int a=29,b=35;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
