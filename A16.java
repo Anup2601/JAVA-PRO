5d24cc93a10dde32fa35f2f1ae56ff153d32102b
Anup Mishra <anupm0873@gmail.com>
2024-05-15 16:11:18 +0530

    array length

---
+++ A16.java
@@ -0,0 +1,14 @@
+//string
+class A16{
+    public static void main(String[] args) {
+        int[] x;
+        x=new int [10];
+        int[] y=new int [5];
+        int[] z=new int[]{1,2,3,4,5,6};
+        int[] w=new int[]{6,7,8,9,0};
+        System.out.println(x.length);
+        System.out.println(y.length);
+        System.out.println(z.length);
+        System.out.println(w.length);
+    }
+}
