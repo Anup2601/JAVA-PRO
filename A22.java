5ea0b51dc9183c0a4454624405d7663aa36e6fa8
Anup Mishra <anupm0873@gmail.com>
2024-05-16 02:09:10 +0530

    string method

---
+++ A22.java
@@ -0,0 +1,26 @@
+class A22{
+    public static void main (String args[]){
+        String s1="Hello Everyone";
+        int s2=s1.length();
+        String s3=s1.toUpperCase();
+        String s4=s1.toLowerCase();
+        char s5=s1.charAt(4);
+        String s6=s1.substring(3);
+        int s7=s1.indexOf("a");
+        String s8=s1.replace("Hello", "Hi");
+        String[] s9=s1.split("e");
+        char[] s10=s1.toCharArray();
+        byte[] s11=s1.getBytes();
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s6);
+        System.out.println(s7);
+        System.out.println(s8);
+        System.out.println(s9);
+        System.out.println(s10);
+        System.out.println(s11);
+
+    }
+}
