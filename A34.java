3be8b0951a08f0119437b64a29e29e4a4c332740
Anup Mishra <anupm0873@gmail.com>
2024-06-08 00:07:47 +0530

    mutliple inharetance

---
+++ A34.java
@@ -0,0 +1,18 @@
+// multiple inharitance 
+class A34{
+    public static void main(String args[]){
+
+    }
+}
+interface  a{
+
+}
+interface  b{
+
+}
+class c{
+
+}
+class d extends c implements a,b{
+
+}
