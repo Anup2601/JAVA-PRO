98111ae7cb9e02d4d9eeee9f1e7545df5c424a06
Anup Mishra <anupm0873@gmail.com>
2024-06-11 01:38:45 +0530

    use of static import

---
+++ A37.java
@@ -0,0 +1,6 @@
+import static pack1.A4.*;
+class A37{
+    public static void main(String[] args) {
+        f1();
+    }
+}
---
+++ pack1/A4.java
@@ -0,0 +1,8 @@
+package pack1;
+
+public class A4 {
+    public static void f1(){
+        System.out.println("asdf1");
+        System.out.println("asdf2");
+    }
+}
