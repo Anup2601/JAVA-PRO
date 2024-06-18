43dea5966685ff203b1277f1bc94e7f5a734c6ea
Anup Mishra <anupm0873@gmail.com>
2024-06-19 03:24:40 +0530

    try and multiple catch

---
+++ A40.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class A40{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] arr={12,23,45,56,78,90};
+        int i,a,ans=0;
+        System.out.println("enter index");
+        try {
+           i=sc.nextInt();
+           System.out.println("enter value"); 
+           a=sc.nextInt();
+           ans=arr[i]/a;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("enter array size less than 7");
+        }catch(ArithmeticException e){
+            System.out.println("enter the value excepet 0");
+        }catch(InputMismatchException e){
+            System.out.println("enter only number");
+        }catch(Exception e){
+            System.out.println("some exception occure");
+        }
+        System.out.println("Answer " + ans);
+    }
+}
