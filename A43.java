2e8b2f31b01fcf50ed519827e2548b3c1cce5c0c
Anup Mishra <anupm0873@gmail.com>
2024-06-20 02:12:15 +0530

    use of cheak exception

---
+++ A43.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class InvalidAgeException extends Exception{
+    public String toString(){
+        return "InvalidAgeException:Valid age should be >18 and <60";
+    }
+}
+
+class A43{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int age;
+        System.out.println("enter your age");
+        age=sc.nextInt();
+        if (age<18||age>60) {
+            System.out.println("you cannot work");
+            InvalidAgeException e=new InvalidAgeException();
+            try {
+                throw e;
+            } catch (Exception ee) {
+            }
+            
+        }
+        System.out.println("A1");
+        System.out.println("A2");
+        System.out.println("A3");
+        System.out.println("A4");
+    }
+}
