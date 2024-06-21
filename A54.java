fd29c674e68d89b60433ef5d858eba3eb6b81ce5
Anup Mishra <anupm0873@gmail.com>
2024-06-21 09:39:47 +0530

    read file data line by line

---
+++ A54.java
@@ -0,0 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class A54{
+    public static void main(String[] args) {
+        try{
+            InputStreamReader i=new InputStreamReader(System.in);
+            BufferedReader b=new BufferedReader(i);
+            String a1=b.readLine();
+            String a2=b.readLine();
+            String a3=b.readLine();
+            System.out.println(a1);
+            System.out.println(a2);
+            System.out.println(a3);
+            // f.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
