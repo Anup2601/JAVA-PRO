1f331ff79c20036ac46184f20dabae765a145a88
Anup Mishra <anupm0873@gmail.com>
2024-06-25 02:22:01 +0530

    anonymous object

---
+++ unit 3/A60.java
@@ -0,0 +1,11 @@
+//anonymous object
+class Hi{
+    void f1(){
+        System.out.println("Hi");
+    }
+}
+class A60{
+    public static void main(String[] args) {
+        new Hi().f1();
+    }
+}
