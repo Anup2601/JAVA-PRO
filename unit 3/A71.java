ca0fe42fa5689cbd21c27210df96089764324346
Anup Mishra <anupm0873@gmail.com>
2024-07-02 23:47:54 +0530

    use of var keyword

---
+++ unit 3/A71.java
@@ -0,0 +1,18 @@
+class A71{
+    public static void main(String[] args) {
+        var x=12;
+        var y=32.32;
+        var z="hello";
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(z.toUpperCase());
+        System.out.println(x+y);
+        var e=new Employee();
+        e.name="anup";
+    }
+}
+class Employee{
+    String name;
+    // var salary  var cannot be use here
+}
