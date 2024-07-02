f7f3bc6a329152ccf9ad4b210755691cd4209145
Anup Mishra <anupm0873@gmail.com>
2024-07-02 23:19:42 +0530

    try with resource

--- unit 3/A69.java
+++ unit 3/A69.java
@@ -1,1 +1,23 @@
- 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+class A69{
+    public static void main(String[] args) {
+        BufferedReader b;
+        try {
+            b=new BufferedReader(new FileReader("A60.java"));
+            while (true) { 
+                String line=b.readLine();
+                if(line==null) break;
+                System.out.println(line);
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }finally{
+            if(b!=null) b.close();//some error
+        }
+
+    }
+}
